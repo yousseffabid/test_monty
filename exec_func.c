d41030e042ed9eaf1920c5e4a163ea5d2ea95db3
yousseffabid <abidyoussef58@gmail.com>
2022-05-11 14:07:16 +0100

    monty

--- exec_func.c
+++ exec_func.c
@@ -3,12 +3,15 @@
  * exec_func - execute function
  * @function_name: name of the function ex: push
  * @line_number: line number of the instruction
+ * @stack: pointer to pointer to stack head
  * Return: void
  */
 void exec_func(char *function_name, unsigned int line_number, stack_t **stack)
 {
-	instruction_t code[] = {{"push", push}, {"pall", pall}, {"pop", pop}, {"pint", pint},
-		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}};
+	instruction_t code[] = {{"push", push}, {"pall", pall},
+							{"pop", pop}, {"pint", pint},
+							{"swap", swap}, {"add", add},
+							{"nop", nop}, {NULL, NULL}};
 	unsigned int i;
 
 	for (i = 0; code[i].opcode != NULL; i++)
